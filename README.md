aecb4d4f15158aec35d6099b720f3b33dccba67f
Skelly98 <tsukiamekun@gmail.com>
2019-11-15 20:27:27 +0100

    clean version but stil bugged when comma

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # PISTL
 Système de traduction de langue naturelle pour les énoncés d’informatique temps-réel 
 
-
 # Installation
 
 - Installer Python 2.7, 3.5, 3.6, ou3.7
 - Installer la libraire nltk : pip install --user -U nltk
 - Dans l'interpréteur python : 
     >> import nltk
+
     >> nltk.download('punkt')
+
     >> nltk.download('averaged_perceptron_tagger')
 
 # Test
--- src/test_nltk.py
+++ src/test_nltk.py
@@ -1,50 +1,14 @@
 import nltk
+import sys
+
 from nltk import WordPunctTokenizer
 
-dico = dict()
-
-def add_elem(d, phrase):
-    tokenizer = WordPunctTokenizer()
-    tokens = tokenizer.tokenize(phrase)
-    tags = nltk.pos_tag(tokens)   
-
-    for (m,t) in tags:
-        if t not in d:
-            if t.isalpha():
-                d[t] = [m]
-        else:
-            if m not in d[t]:
-                d[t].append(m)
-    return d
-
-phrase = "When an error message is displayed the only available user action is acknowledgement via the ok button"
-phrase2 = "When the cancel button on the identify traveler screen is pressed control returns to the main menu screen" 
-phrase3 = "When a match is found all fields are filled in"
-phrase4 = "When a connection is made to the POP server, mail will be transferred to the local machine "
-phrase5 = "When the name of a mailbox is double-clicked, that mailbox will be opened"
-
-dico = add_elem(dico,phrase4)
-
-def add_string(tag,dico):
-    if len(dico[tag]) > 0:
-        res = tag+" -> "
-        for n in dico[tag]:
-            res+="""'"""+n+"""'"""+"| "
-        return res[:-2]+"\n"
-    else:
-        return ""
-
-def add_tags(dico):
-    res =""
-    for t in dico:
-        res+=add_string(t,dico)
-    return res
- 
-
-
+#definition of the grammar for parsing the tokens 
 grm = """
-  L -> NP VP
-  NP -> DT NOMS PP | DT RBS JJS NOMS | DT JJS NOMS | DT NOMS | NOMS
+  S -> COND RES | COND | RES
+  COND -> WRB NP VP 
+  RES -> NP VP
+  NP -> DT NOMS PP | DT RBS JJS NOMS | DT JJS NOMS | DT NOMS | NOMS 
   VP -> VERB VBN | VERB CO | VERB VBN CO | MD VB | MD VB VBN | MD VB VBN CO
   VERB -> VBZ | VBP 
   COD -> JJ | JJ PP
@@ -57,38 +21,111 @@ grm = """
   NOMS -> NOM NOMS | NOM
   NOM -> NN | NNS | NNP | NNPS \n"""
 
-grammar1 = nltk.CFG.fromstring("""
-     S -> WRB L L | WRB L L\n"""+
-  grm+add_tags(dico))
+#remove unneccessary characters in the sentence such as '.', '-' that may induce confusion to the nltk tool
+def purge(sentence):
+    remove_hyphen = sentence.split('-')
+    no_hyphen = ''.join(remove_hyphen)
+    remove_dot = no_hyphen.split('.')
+    no_dot = ''.join(remove_dot)
+    remove_apostrophe =  no_dot.split('\'')
+    no_apostrophe = ''.join(remove_apostrophe)
+    ret = no_apostrophe
+    return ret
 
-def with_comma(p):
-    dico = dict()
-    tokens = nltk.word_tokenize(p)
-    dico = add_elem(dico,p)
-    c = -1
+#return the position of the comma character ',' if it exists
+def get_comma_position(tokens):
+    c=-1
     for i in range(0,len(tokens)-1):
         if(tokens[i] == ','):
             c = i
             break
-    
-    if c!= -1:
-        grammar2 = nltk.CFG.fromstring(grm+add_tags(dico))
-        rd_parser = nltk.RecursiveDescentParser(grammar2)
+    return c
+
+#tokenize the sentence
+def tokenize(sentence):
+    tokenizer = WordPunctTokenizer()
+    tokens = tokenizer.tokenize(sentence)
+    return tokens
+
+# add an word as an terminal symbol to the dictionnary in their rightful labels
+def add_elem(d,tags):
+    for (w,t) in tags:
+        if t not in d:   
+            if t.isalpha():
+                d[t] = [w]
+        else:
+            if w not in d[t]:
+                d[t].append(w)
+    return d
+
+#add a line of rules to the grammar for a specific label 
+def add_rule(tag,dico):
+    if len(dico[tag]) > 0:
+        res = tag+" -> "
+        for n in dico[tag]:
+            res+="""'"""+n+"""'"""+"| "
+        return res[:-2]+"\n"
+    else:
+        return ""
+
+#create a string containing the set of rules of all terminal symbols we found in the sentence 
+def add_all_rules(dico):
+    res =""
+    for t in dico:
+        res+=add_rule(t,dico)
+    return res
+
+#generate the parse tree
+def parse(dico,tokens):
+    tags = nltk.pos_tag(tokens)   
+    dico = add_elem(dico,tags)
+
+
+    # first we add all the terminals symbols to the grammar
+    grammar = nltk.CFG.fromstring(grm+add_all_rules(dico))
+     # then we execute a bottom up parsing 
+    rd_parser = nltk.BottomUpChartParser(grammar)
+
+    c = get_comma_position(tokens)
+    # if there is a comma, we remove it (I think we could remove it earlier in the purge)
+    if c != -1:
         trees = []
-        for tree in rd_parser.parse(tokens[1:c]):
+
+        for tree in rd_parser.parse(tokens[0:c]):
             trees = trees + [tree]
+        
+        print(trees)
+
         for tree in rd_parser.parse(tokens[c+1:]):
             trees = trees + [tree]
 
-        tree1 = nltk.Tree('WRB', ['When'])
-        tree = nltk.Tree('S',[tree1]+trees)
-        tree.draw()
+        print(trees)
+    else: 
+        for tree in rd_parser.parse(tokens):
+            tree.draw() 
+        
 
+#set of examples for unit testing
+example = {
+    "1" :  "When an error message is displayed the only available user action is acknowledgement via the 'ok' button",
+    "2" :  "When the cancel button on the identify traveler screen is pressed control returns to the main menu screen" ,
+    "3" :  "When a match is found all fields are filled in",
+    "4" : "When a connection is made to the POP server, mail will be transferred to the local machine",
+    "5" : "When the name of a mailbox is double-clicked, that mailbox will be opened.",
+}
 
+def main(argv):
 
-sent = phrase2.split()
-rd_parser = nltk.RecursiveDescentParser(grammar1)    
-#for tree in rd_parser.parse(sent):
- #   tree.draw() 
+    sentence = example.get(str(argv[1]))
 
-with_comma(phrase4)
+    #create dict 
+    dico = dict()
+
+    #lexical analysis
+    pure_sentence = purge(sentence)
+    tokens = tokenize(pure_sentence)
+
+    #syntax analysis
+    parse(dico,tokens)
+
+main(sys.argv)
